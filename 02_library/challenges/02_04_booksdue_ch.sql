df01145bf3c55958bd51f77408a58401cd03bed5
Stijn-weather <132081310+Stijn-weather@users.noreply.github.com>
2023-05-03 15:09:19 +0000

    threefour

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,7 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+SELECT Title, Firstname, Email FROM Loans
+JOIN Books ON Books.BookID = Loans.BookID
+JOIN Patrons ON Patrons.PatronID = Loans.PatronID
+WHERE Loans.DueDate LIKE "2022-07-13" AND Loans.ReturnedDate IS NULL
