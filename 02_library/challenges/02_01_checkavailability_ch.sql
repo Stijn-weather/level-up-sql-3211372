6cbab87be36a0fc6dfea405a9292469f125aaade
Stijn-weather <132081310+Stijn-weather@users.noreply.github.com>
2023-05-03 14:05:33 +0000

    Too

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,22 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+SELECT COUNT(BookID) FROM BOOKS
+WHERE Title = "Dracula"
+
+SELECT COUNT(LoanID) FROM Loans
+JOIN Books ON Books.BookID = Loans.BookID
+WHERE Books.Title="Dracula"
+AND Loans.ReturnedDate IS NULL
+
+SELECT(
+  SELECT COUNT(BookID)
+  FROM BOOKS
+  WHERE Title = "Dracula") 
+  - (SELECT COUNT(LoanID)
+    FROM Loans
+    JOIN Books ON Books.BookID = Loans.BookID
+    WHERE Books.Title="Dracula"
+    AND Loans.ReturnedDate IS NULL)
+    AS
+    Hoottenanny
--- 02_library/challenges/02_02_addbook_ch.sql
+++ 02_library/challenges/02_02_addbook_ch.sql
@@ -9,3 +9,11 @@
 -- Author: Jonathan Swift
 -- Published: 1729
 -- ID Number: 4899254401
+
+INSERT INTO Books(Title, Author, Published, Barcode)
+VALUES('Dracula', 'Bram Stoker', '1897', '4819277482'),('Gulliver
+’s Travels into Several Remote Nations of the World','Jonathan Swift','1729','4899254401')
+
+
+DELETE FROM Books
+WHERE (BookID LIKE '20%') AND BookID != '200' AND BookID != '20'
